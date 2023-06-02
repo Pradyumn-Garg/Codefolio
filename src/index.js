26709a5b4b75bf4ba32f29c58a74d490c20403b2
Pradyumn Garg <86672147+Pradyumn-Garg@users.noreply.github.com>
2023-06-03 05:13:50 +0530

    landing page created
    connected to express.js initial web scraping backend api.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Mainpage from './mainpage';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Mainpage />}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
