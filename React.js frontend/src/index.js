b8c0d5da47c76bf8a737b53f09ac9676a683941d
Aditya <adityabisht02@gmail.com>
2023-12-06 00:25:39 +0530

    removed tailwind

--- React.js frontend/src/index.js
+++ React.js frontend/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import LandingPage from "./components/LandingPage";
 import CombinedTemplate from "./components/portfolios/CombinedTemplate";
-import ProjectTemplate from "./components/portfolios/ProjectTemplate";
 import LeetcodeTemplate from "./components/portfolios/LeetcodeTemplate";
 import ChooseTemplate from "./components/ChooseTemplate";
 import reportWebVitals from "./reportWebVitals";
@@ -19,7 +18,7 @@ root.render(
           <Route path="/" element={<LandingPage />} />
           <Route path="/combinedtemplate" element={<CombinedTemplate />} />
           <Route path="/choosetemplate" element={<ChooseTemplate />} />
-          <Route path="/projecttemplate" element={<ProjectTemplate />} />
+
           <Route path="/leetcodetemplate" element={<LeetcodeTemplate />} />
         </Routes>
       </GlobalContextProvider>
